c635e6f075282e7daa3154d413ac7752e5ee70e3
marcoderso <104020027+marcoderso@users.noreply.github.com>
2025-12-06 10:28:06 +0100

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import SnakeGame from "./SnakeGame";
+
+export default function Home() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "20px",
+      }}
+    >
+      <SnakeGame />
+    </main>
+  );
+}
